b2eb67102bfe5112fa0f4feb97eb9c3c5f3b1a60
lien <longyuhuhao@126.com>
2019-06-19 08:43:04 +0800

    Update array.js

--- leetcode/array.js
+++ leetcode/array.js
@@ -155,3 +155,19 @@ class Heap {
     }
   }
 }
+
+var maxProfit = function (prices) {
+  let count = 0;
+  for (let i = 0, len = prices.length; i < len; i++) {
+    for (let j = i; j < len -1; j++) {
+      if (prices[j+1] > prices[j]) {
+        count += prices[j+1] - prices[j]
+        i = j
+      } else {
+        i = j
+        break
+      }
+    }
+  }
+  return count 
+};
