3b4bb43048356bf72cfabf4f59b61fcfc21edcd7
lien <longyuhuhao@126.com>
2019-12-01 14:51:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import stop from './util'
-function FirstNotRepeatingChar (str) {
-  let arr = str.split("")
-  for (let i = 0; i < arr.length; i++) {
-    if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
-      return i
+function trap (height) {
+  let res = 0
+  let letfMax = []
+  let rightMax = []
+  for (let i =1; i < height.length - 1; i++) {
+    letfMax[i] = Math.max(letfMax[i -1], height[])
+  }
+  for (let i=1; i < height.length - 1; i++) {
+    letfMax = Math.max(...height.slice(0,i))
+    rightMax = Math.max(...height.slice(i))
+    debugger
+    if (Math.min(letfMax, rightMax) - height[i]>0) {
+      res += Math.min(letfMax, rightMax) - height[i]
     }
   }
-   return false 
+  return res
 }
 
-const str = "qwertyuiopsdfghjklzxcvbnmwer"
-console.log(FirstNotRepeatingChar(str))
+const height = [2, 0, 2]
+console.log(trap(height,3))
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,6 +13,7 @@ module.exports =  {
       loader: 'babel-loader'
     }]
   },
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
