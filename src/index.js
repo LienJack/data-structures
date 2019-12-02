d611372a57a20c26a7d57eb91b2966a629d404e2
lienjie <longyuhuhao@126.com>
2019-12-02 20:24:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
-var getIntersectionNode = function(headA, headB) {
-    let lengthA = 0
-    let lengthB = 0
-    let curA = headA
-    let curB = headB
-    let long
-    let short
-    let lang
-  
-    while (curA) {
-      lengthA++
-      curA = curA.next
-    }
-    while (curB) {
-      lengthB++
-      curB = curB.next
-    }
-    
-    if (lengthA > lengthB) {
-      long = headA
-      short = headB
-      lang = lengthA - lengthB
-    } else {
-      long = headB
-      short = headA
-      lang = lengthB - lengthA
-    }
-
-    while (lang) {
-      long = long.next
-      lang --
-    }
-    while(short) {
-      if (short === long) return short
-      short = short.next
-      long = long.next
-    }
-    return null
+var mergeTwoLists = function(l1, l2) {
+  let head
+  if (!l1) return l2
+  if (!l2) return l1
+  if (l1.val < l2.val) {
+    head = l1
+    head.next = mergeTwoLists(l1.next, l2)
+  } else {
+    head = l2
+    head.next = mergeTwoLists(l1, l2.next)
+  }
+  return head
 };
