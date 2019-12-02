d9b4506126fcaeddce323dd30826f001a03c4386
lienjie <longyuhuhao@126.com>
2019-12-02 16:50:47 +0800

    udpate

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
-import stop from './util'
-import { LinkedNode, LinkedList } from '../collection'
+var getIntersectionNode = function(headA, headB) {
+    let lengthA = 0
+    let lengthB = 0
+    let curA = headA
+    let curB = headB
+    let long
+    let short
+    let lang
+  
+    while (curA) {
+      lengthA++
+      curA = curA.next
+    }
+    while (curB) {
+      lengthB++
+      curB = curB.next
+    }
+    
+    if (lengthA > lengthB) {
+      long = headA
+      short = headB
+      lang = lengthA - lengthB
+    } else {
+      long = headB
+      short = headA
+      lang = lengthB - lengthA
+    }
 
-var copyRandomList = function(head) {
-  if (!head) return null
-  let cur = head
-  let cloneHead = new Node
-  let tmp = cloneHead
-  let map = new Map()
-
-  while(cur) {
-    tmp.val = cur.val
-    tmp.next = cur.next ? new Node : null
-    map.set(cur, tmp)
-    tmp = tmp.next
-    cur = cur.next
-  }
-  tmp = cloneHead
-  while (head) {
-    tmp.random = head.random ? map.get(head.random): null
-    head = head.next
-    tmp = tmp.next
-  }
-  return cloneHead
+    while (lang) {
+      long = long.next
+      lang --
+    }
+    while(short) {
+      if (short === long) return short
+      short = short.next
+      long = long.next
+    }
+    return null
 };
-
-
-
