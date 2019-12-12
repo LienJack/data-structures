9fe34d99274b4ae8d3e69f223f0db05274150616
lienjie <longyuhuhao@126.com>
2019-12-12 11:39:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-const coins = [1, 2, 5]
-var coinChange = function (coins, amount) {
-  if (amount < 1) return 0;
-  var dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (var i = 0; i <= amount; i++) {
-    for (var r = 0; r < coins.length; r++) {
-      if (coins[r] <= i) {
-        dp[i] = Math.min(dp[i], dp[i - coins[r]] + 1);
-      }
-    }
+const s = 'babad'
+var longestPalindrome = function(s) {
+  if(!s || s.length < 2){
+      return s;
   }
-  return dp[amount] > amount ? -1 : dp[amount];
+  let start = 0,end = 0;
+  let n = s.length;
+  // 中心扩展法
+  let centerExpend = (left,right) => {
+      while(left >= 0 && right < n && s[left] == s[right]){
+          left--;
+          right++;
+      }
+      return right - left - 1;
+  }
+  for(let i = 0;i < n;i++){
+      let len1 = centerExpend(i,i);
+      let len2 = centerExpend(i,i+1);
+      // 两种组合取最大回文串的长度
+      let maxLen = Math.max(len1,len2);
+      if(maxLen > end - start){
+          // 更新最大回文串的首尾字符索引
+          start = i - ((maxLen - 1) >> 1);
+          end = i + (maxLen >> 1);
+      }
+  }
+  return s.substring(start,end+1);
 };
-console.log(coinChange(coins,11))
+
+console.log(longestPalindrome(s))
