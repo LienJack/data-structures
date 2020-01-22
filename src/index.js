3d153e3961fc64613a57495788b6b79c850cc203
lien <longyuhuhao@126.com>
2020-01-22 16:10:12 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-var maxArea = function (height) {
-  let max = 0
-  let i = 0
-  let j = height.length - 1
-  while (i < j) {
-    let minHeight = height[i] < height[j] ? height[i++] : height[j--]
-    let area = (j - i + 1) * minHeight
-    max = Math.max(max, area)
+var threeSum = function (nums) {
+  let res = []
+  nums = nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i-1]) continue // 这里疑惑
+    let j = i + 1
+    let k = nums.length - 1
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k] 
+      if (sum === 0) {
+        res.push([ nums[i], nums[j], nums[k] ])
+        while (j < k && nums[j] === nums[++j]);
+        while (j < k && nums[k] === nums[--k]);
+      } else if (sum < 0) {
+        while (j < k && nums[j] === nums[++j]);
+      } else {
+        while (j < k && nums[k] === nums[--k]);
+      }
+    }
   }
-  return max
-};
-const arr = [1, 1]
+  return res
+}
 
-console.log(maxArea(arr))
+const arr = [-1, 0, 1, 2, -1, -4]
+
+console.log(threeSum(arr))
