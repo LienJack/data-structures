39f62438803bed274691c8a15ed4099b3eecc367
LienJack <longyuhuhao@126.com>
2020-01-24 12:43:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-var threeSum = function (nums) {
-  let res = []
-  nums = nums.sort((a, b) => a - b)
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i-1]) continue // 这里疑惑
-    let j = i + 1
-    let k = nums.length - 1
-    while (j < k) {
-      let sum = nums[i] + nums[j] + nums[k] 
-      if (sum === 0) {
-        res.push([ nums[i], nums[j], nums[k] ])
-        while (j < k && nums[j] === nums[++j]);
-        while (j < k && nums[k] === nums[--k]);
-      } else if (sum < 0) {
-        while (j < k && nums[j] === nums[++j]);
+var groupAnagrams = function(strs) {
+    let map = new Map()
+    strs.forEach(item => {
+      let getKey = item.split("").sort().join("")
+      if (map.has(getKey)) {
+        map.get(getKey).push(item)
       } else {
-        while (j < k && nums[k] === nums[--k]);
+        map.set(getKey, [item])
       }
-    }
-  }
-  return res
-}
-
-const arr = [-1, 0, 1, 2, -1, -4]
-
-console.log(threeSum(arr))
+    }) 
+    return [...map.values()]
+};
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+console.log(groupAnagrams(strs))
