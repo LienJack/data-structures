0d4ead793840addc24059374819ae26afb9a05d7
LienJack <longyuhuhao@126.com>
2020-02-01 20:01:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-var lastStoneWeight = function(stones) {
-    if(!stones.length) return 0
-    if(stones.length<2) return stones[0]
-    var DF = () => {
-        stones.sort((a,b)=> a-b)
-        console.log(stones)
-        let y = stones.pop()
-        let x = stones.pop()
-        if(x!==y) {
-            stones.push(y-x)
+var minDistance = function(word1, word2) {
+    let row = word2.length
+    let col = word1.length
+    let dp = Array.from(Array(row+1), () => Array(col+1).fill(0))
+    for (let i = 0; i <= col; i++) {
+        dp[0][i] = i
+    } 
+    for (let i = 0; i <= row; i++) {
+        dp[i][0] = i
+    } 
+    for (let i = 1; i <= row; i++) {
+        for (let j = 1; j <= col; j++) {
+            if (word1[j-1] === word2[i-1]) {
+                dp[i][j] = dp[i-1][j-1]
+            } else {
+                dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+            }
         }
     }
-    while(stones.length > 1) {
-        DF()
-    }
-    return stones.length ?  stones[0] : 0
+    console.log(dp)
+    return dp[row][col]  
 };
+
+
+
+console.log(minDistance("horse","ros"))
