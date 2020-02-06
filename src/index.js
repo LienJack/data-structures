33ad10f18b700b888922db36dd30b3bf570f1592
LienJack <longyuhuhao@126.com>
2020-02-06 11:29:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-var findCircleNum = function(M) {
-    if (!M.length) return 0
-    let len = M.length
-    let count = 0
-    let visited = {}
-    function DFS (i) {
-        for (let j = 0; j < len; j++) {
-            if (M[i][j] === 1 && !visited[j]) {
-                visited[j] = true
-                DFS(j)
-            }
-        }
+var trap = function(height) {
+    let res = 0
+    let len = height.length
+    let leftMaxArr = Array(len).fill(0)
+    let rightMaxArr = Array(len).fill(0)
+    leftMaxArr[0] = height[0]
+    rightMaxArr[len-1] = height[len-1]
+    for (let i = 1; i < len; i++) {
+        leftMaxArr[i] = Math.max(leftMaxArr[i-1], height[i])
     }
-    for (let i = 0; i < len; i++) {
-        if (!visited[i]) {
-            DFS(i)
-            count ++
-        }
+    for (let i = len - 2; i > 0; i--) {
+        rightMaxArr[i] = Math.max(rightMaxArr[i+1], height[i])
     }
-    return count
+
+    for (let i = 1; i < len - 2; i ++) {
+        let minHeight = Math.min(leftMaxArr[i], rightMaxArr[i])
+        let def = minHeight - height[i]
+        if (def > 0 ) res += def 
+    }
+
+    return res
+    
 };
-const nums = 
-[[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]
-console.log(findCircleNum(nums))
+
+const arr = [0,1,0,2,1,0,1,3,2,1,2,1]
+console.log(trap(arr))
