b555288a339e0a18108a7a67e05000972c9a06cc
lien <longyuhuhao@126.com>
2020-02-09 11:26:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-var fn = () => {
-    let flag = true
-    return () => {
-        if (flag) {
-            console.log('1')
+var sortList = function(head) {
+    if (!head || !head.next) return head 
+    let mid = head
+    let cur = head.next
+    let left
+    let right
+    let leftHead
+    let rightHead
+    while(cur) {
+        if (mid.val > cur.val) {
+            if(leftHead) {
+                left.next = cur
+            } else {
+                leftHead = cur
+                left = cur
+            }
+            cur = cur.next
         } else {
-            console.log('2')
+            if (rightHead) {
+                right.next = cur
+            } else {
+                rightHead = cur
+                right = cur
+            }
+            cur = cur.next
         }
-        flag = !flag
     }
-}
-
-const test = fn()
-
-test()
-test()
+    let resLeft = sortList(leftHead)
+    let resRight = sortList(rightHead)
+    let temp = resLeft
+    let pre = null
+    while(temp) {
+        pre = temp
+        temp = resLeft.next
+    }
+    pre.next = mid
+    mid.next = resRight 
+    return resLeft
+};
