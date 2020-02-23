211b1cf035f6a0dc3556f3355c0c5d54569610ff
lien <longyuhuhao@126.com>
2020-02-23 15:00:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-function maxSlidingWindow(arr, k) {
-  let res = []
-  let window = []
-  for (let i = 0; i < arr.length; i++) {
-    if (window.length < k) {
-      window.push(arr[i])
-      if (window.length === k) {
-        res.push(Math.max(...window))
-      }
-    } else {
-      window.shift()
-      window.push(arr[i])
-      console.log([].concat(...window))
-      res.push(Math.max(...window))
-    }
-  }
-  return _arr
-}
-function randomRange(myMin, myMax) {
-  return Math.floor(Math.random()*(myMax - myMin + 1)) + myMin; 
-}
-let arr = Array.from(Array(10), (item, index) => index)
-console.log(randoSort(arr))
+var reverseWords = function (s) {
+  return s.split(" ").filter(item => item).reverse().join(" ")
+};
+console.log(reverseWords("the sky is blue"))
