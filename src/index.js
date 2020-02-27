1f3bf17f3477ecf0bd821d7f99b0688694541662
lien <longyuhuhao@126.com>
2020-02-27 23:13:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-async function async1() {
-  console.log('async1 start');
-  await async2();
-  console.log('async1 end');
+Function.prototype.myBind = function (context = window, ...args1) {
+  const _this = this
+  let fn = Symbol()
+  let newFn = function (...args2) {
+    let curContext
+    if (!new.target) {
+      curContext = context
+    } else {
+      curContext = this
+    }
+    curContext[fn] = _this
+    let res = curContext[fn](...args1, ...args2)
+    delete curContext[fn]
+    return res
+  }
+  newFn.prototype = Object.create(_this.prototype)
+  return newFn
 }
-async function async2() {
-  console.log('async2');
+
+function Animal(name, color) {
+  this.name = name;
+  this.color = color;
 }
-console.log('script start');
-setTimeout(function () {
-  console.log('setTimeout');
-}, 0)
-async1();
-new Promise(function (resolve) {
-  console.log('promise1');
-  resolve();
-}).then(function () {
-  console.log('promise2');
-});
-console.log('script end');
+Animal.prototype.say = function () {
+  return `I m a ${this.color} ${this.name}`;
+};
+const Cat = Animal.myBind(null, 'cat');
+const cat = new Cat('white');
+if (cat.say() === 'I m a white cat' && cat instanceof Cat && cat instanceof Animal) {
+  console.log('success');
+}
--- 头条/模仿/bind.js
+++ 头条/模仿/bind.js
@@ -1,11 +1,33 @@
-// 本质obj.fn
 Function.prototype.myBind = function (context = window, ...args1) {
   const _this = this
-  return function F(...args2) {
-    // 判断是否用于构造函数
-    if (this instanceof F) {
-      return new _this(...args1, ...args2)
+  let fn = Symbol()
+  let newFn = function (...args2) {
+    let curContext
+    if (!new.target) {
+      curContext = context
+    } else {
+      curContext = this
     }
-    return _this.apply(context, args1.concat(args2))
+    curContext[fn] = _this
+    let res = curContext[fn](...args1, ...args2)
+    delete curContext[fn]
+    return res
   }
+  newFn.prototype = Object.create(_this.prototype)
+  return newFn
+}
+
+
+// 测试用例
+function Animal(name, color) {
+  this.name = name;
+  this.color = color;
+}
+Animal.prototype.say = function () {
+  return `I m a ${this.color} ${this.name}`;
+};
+const Cat = Animal.myBind(null, 'cat');
+const cat = new Cat('white');
+if (cat.say() === 'I m a white cat' && cat instanceof Cat && cat instanceof Animal) {
+  console.log('success');
 }
